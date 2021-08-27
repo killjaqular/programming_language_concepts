fdfac7592c709b2b6e1abb221ba6cf6cb7921bab
Adonay <adonaypichardo@gmail.com>
2021-08-27 06:31:06 -0700

    need to read in an unknown number of lines, find how to append to an array

--- project1/HelloWorld.f95
+++ project1/HelloWorld.f95
@@ -1,5 +1,20 @@
 program helloworld
 
+	implicit none
+	integer :: stdin  = 5
+	integer :: stdout = 6
+	integer, dimension(0:2) :: input_integers
+	real,    dimension(0:2) :: input_reals
+
+	integer :: i
+	do i = UBOUND(input_integers, dim=1), LBOUND(input_integers, dim=1), -1
+		read (*, *) input_integers(i), input_reals(i)
+	end do
+
+	do i = UBOUND(input_integers, dim=1), LBOUND(input_integers, dim=1), -1
+		write (stdout, stdout) input_integers(i), input_reals(i)
+	end do
+
 	print *,'Hello World!'
 
 end program helloworld
