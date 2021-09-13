f5cedf39d4530bc624c0fa9b4be285ff6b162a8b
Adonay <adonaypichardo@gmail.com>
2021-09-13 17:03:05 -0400

    implemented correct level of accurracy

--- project1/project1.f95
+++ project1/project1.f95
@@ -5,38 +5,40 @@
 ! This program compiles with gfortran version:
 ! GNU Fortran (Ubuntu 5.4.0-6ubuntu1~16.04.10) 5.4.0 20160609
 
-FUNCTION f_lambert(z)
+REAL*16 FUNCTION f_lambert(z)
 ! Newtonian approximation of the Lambert W function at z
-    REAL, intent(IN) :: z ! Used as input
+    REAL*16, intent(IN) :: z ! Used as input
 
-    REAL :: small_change  ! Used to measure change between iterations
-    REAL :: approx        ! A variable used to approximate the value at z
-    REAL :: old_value     ! Holds last iteration's approx
+    REAL*16 :: small_change  ! Used to measure change between iterations
+    REAL*16 :: approx        ! A variable used to approximate the value at z
+    REAL*16 :: old_value     ! Holds last iteration's approx
 
-    small_change = 0.000000001 ! Level of accuracy
-    approx       = 1           ! Initial guess
+    small_change = 0.0000000001 ! Level of accuracy
+    approx       = 1.0          ! Initial guess
     DO WHILE (ABS(old_value - approx) > small_change)
         old_value = approx
 
         approx = approx - ((approx * exp(approx) - z) / (exp(approx) + approx * exp(approx)))
     END DO
 
-    print *, "z", z, " = ", approx
-
-    f_lambert = approx ! Return the estimated value
+    f_lambert = approx
 END FUNCTION
 
-FUNCTION f_gamma(alpha)
+REAL*16 FUNCTION f_gamma(alpha)
 ! Calculates the gamma value used to find total population infected
-    REAL, intent(IN) :: alpha ! Used as input
+    REAL*16, intent(IN) :: alpha ! Used as input
+
+    REAL*16 :: f_lambert
 
     f_gamma = 1 + f_lambert(-alpha * exp(-alpha)) / alpha
 END FUNCTION
 
-FUNCTION f_total_infected(alpha, population)
+REAL*16 FUNCTION f_total_infected(alpha, population)
 ! Calculates the total population infected
-    REAL,    intent(IN) :: alpha
-    INTEGER, intent(IN) :: population
+    REAL*16, intent(IN) :: alpha
+    REAL*16, intent(IN) :: population
+
+    REAL*16 :: f_gamma
 
     f_total_infected = f_gamma(alpha) * population
 END FUNCTION
@@ -48,21 +50,21 @@ PROGRAM project1
 ! and 'a' as the number of people one infected person is in contact with.
 ! T               =          f_gamma(a) * n: Total number of people infected.
 ! infected_ration =                   T / n: Ratio of infected to not infected.
-! f_lambert(z)     = Newtonian Approximation: Using Newtonian Approximation,
+! f_lambert(z)    = Newtonian Approximation: Using Newtonian Approximation,
 ! we have estimated the value of the Lambert W function at the given z value.
 
     IMPLICIT NONE ! Do not allow implicit casting
     ! Functions
-    REAL :: f_total_infected  ! Calculates total population infected
+    REAL*16 :: f_total_infected ! Calculates total population infected
 
     ! Used for input
-    INTEGER          :: n                 ! Initial population
-    REAL :: a                 ! Initial in contact with infected person
+    REAL*16 :: n ! Initial population
+    REAL*16 :: a ! Initial in contact with infected person
 
     ! Used for output
-    REAL :: T                 ! Total population infected
-    REAL :: infected_ratio    ! T/n
-    INTEGER          :: case_number       ! Current case number
+    REAL*16    :: T              ! Total population infected
+    REAL*16    :: infected_ratio ! T/n
+    INTEGER :: case_number    ! Current case number
 
     ! Used for control, auxiliary
     INTEGER :: flag ! Used to signal EOF has been read
@@ -83,7 +85,7 @@ PROGRAM project1
 
         ! Print results
         case_number = case_number + 1
-        print *, "Case", case_number, ":", INT(T), INT(infected_ratio), "%"
+        print *, "Case", case_number, ":", NINT(T), NINT(infected_ratio), "%"
     END DO
 
 END PROGRAM project1
